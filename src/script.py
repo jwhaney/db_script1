bd3deff1d1712a980fc6e9119d870481fbc0fe9f
john <13525011+jwhaney@users.noreply.github.com>
2022-02-02 13:07:39 -0600

    test script works now

--- src/script.py
+++ src/script.py
@@ -1,6 +1,6 @@
 # hello world test program using WSL and VSCode for native linux dev feel, ya!
 
-class Doer(object):
+class Dostuff(object):
 
     def __init__(self, h, ex, uhoh, a, str_in):
         self.h = h
@@ -9,15 +9,16 @@ class Doer(object):
         self.a = a
         self.str_in = str_in
 
-        print('is this working')
+        self.requestor(str_in)
     
     def requestor(self, str_in):
-        print('entering requestor method')
         # main print statement
         print('would you like to see what I have to say? Type "y" for yes and "n" for no.')
+        str_in = str(input())
+        
         print('length of string input is:', len(str_in))
         # send str_in to resolver method
-        self.resolver(str_in)
+        self.resolver(h, ex, uhoh, a, str_in)
 
     def resolver(self, h, ex, uhoh, a, str_in):
         if len(str_in) == 1:
@@ -28,22 +29,22 @@ class Doer(object):
                     print(ex)
                 else:
                     print(uhoh)
-                    self.requestor
+                    self.requestor()
             else:
                 print('that string is not in the list of acceptable answers. please try again.')
-                self.resolver()
+                self.requestor(str_in)
         else:
             print('only one letter response is allowed. please try again.')
-            self.resolver()
+            self.requestor(str_in)
 
 # start the script
 if __name__ == "__main__":
-    h = 'hello, world!'
-    ex = 'bah bye! exiting...'
-    uhoh = 'uh oh, you entered an invalid value. please try again.'
+
+    h = "HELLO, world!"
+    ex = "bah bye! exiting..."
+    uhoh = "uh oh, you entered an invalid value. please try again."
     a = ['Y', 'y', 'N', 'n']
-    str_in = str(input())
 
-    print('is this working?')
+    str_in = ""
 
-    script = Doer(h, ex, uhoh, a, str_in)
+    script = Dostuff(h, ex, uhoh, a, str_in)
